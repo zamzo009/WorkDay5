6483d3e3f82231e1949d83ead576656aba6c7c74
idsc3101 <idsc3101@gmail.com>
2013-10-07 17:24:34 -0500

    Work day 5

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -5,11 +5,20 @@ Public Class Form1
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
         Dim name As String
         name = txtFullName.Text
-        txtFirstName.Text = FirstName(name)
+
+        Dim result As String
+        result = FirstName(name)
+        txtFirstName.Text = result
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        Dim idx As Integer
+        idx = name.IndexOf(" ")
+
+        Dim fName As String
+        fName = name.Substring(0, idx)
+
+        Return fName
     End Function
 
 End Class
--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,25 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        Dim x As Double
+        Dim y As Double
+        Dim z As Double
 
-        txtHyp.Text = "Not Implemented Yet"
+        'read the input from the user
+        x = CDbl(txtSideOne.Text)
+        y = CDbl(txtSideTwo.Text)
+
+        z = Hypotenuse(x, y)
+
+        txtHyp.Text = CStr(z)
 
     End Sub
+
+    Function Hypotenuse(ByVal a As Double, b As Double) As Double
+        Dim c As Double
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+
+        Return c
+    End Function
 
 End Class
